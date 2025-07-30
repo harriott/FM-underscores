d1a56b615102723d2d52d6550ca04bc5e960ccd2
Joseph Harriott <trohib@gmail.com>
2025-07-30 17:41:04 +0200

    dots in dirs

--- underscores.pl
+++ underscores.pl
@@ -1,4 +1,7 @@
 #!/usr/bin/perl
+
+# Joseph Harriott - https://harriott.github.io/ - Wed 30 Jul 2025
+
 # This script looks recursively at all files and directories in the current directory,
 # and prints out the list found, but with these changes:
 # - File (not directory) extensions: lowercased, and any leading blankspaces removed
@@ -11,65 +14,78 @@
 
 # Prerequisites:  a system with Perl on.
 
-# Drop this file into the parent folder that you want to work on, open a Terminal there,
-# enter the name of this file, hit return, and watch the progress!
-# Alternatively, something like:  perl /mnt/BX200/Files/IT_stack/underscores/underscores.pl
+# in the directory to change,  perl $onGH/FM-underscores/underscores.pl
 
-# Accent in filename, like `é` is handled correctly,
-# though the changes reported in a Windows console may look odd.
+# Accents in filename, like `é` are handled correctly,
+# though the changes reported in a Windows console may look odd
+# and I now change out accented stuff - see below.
 
 # Caveat: this may silently fail to rename folders monitored by Dropbox.
 
-use strict;  use warnings;  use File::Basename;  use File::Copy 'move';  use File::Find;
+use strict;  use warnings;  use File::Basename;  use File::Copy 'move';
+use File::Find; # finddepth()
 
 # First, create a time check:
 END { print "This Perl program ran for ", time() - $^T, " seconds.  All changes reported.\n"}
 
 # Next, warn what's about to be done:
-print "You are about to recursively rename files and directories: spaces to underscores!\n";
-print "This may be difficult to reverse!\n";
-print "Enter g to go ahead, s to simulate, or just Enter to quit: ";
+print "You are about to recursively rename files and directories!  Enter  to quit, or  g  to go ahead: ";
 my $resp = <STDIN>;
 chomp $resp; # Get rid of newline character at the end
-if ($resp ne "g") {
-	if ($resp eq "s") {
-		print "Simulating...\n";
-	} else {
-		print "Quit!  ";
-		exit 0;
-	}
-} else {
-print "Working...\n";
-}
+if ($resp ne "g") { print "Quit!  "; exit 0; } else { print "Working...\n"; }
 
 # Now do all the work:
 print "Okay, these are the original branches with their renamed leafs:\n";
-finddepth({ wanted => \&allfods, no_chdir => 1 }, ".");   # finished!
-if ($resp eq "s") {
-	print "hit Enter to quit";
-	<STDIN>;
-}
+finddepth({ wanted => \&allds, no_chdir => 1 }, "."); # dots in dirs
+finddepth({ wanted => \&allds, no_chdir => 1 }, "."); # dots in lower dirs
+finddepth({ wanted => \&allfods, no_chdir => 1 }, ".");
 
-#  The subroutine to adjust all file or directory names:
+sub allds {
+  if (-d $_) {
+    if (length($_) > 1) {
+      my $dnf = $_;
+      $dnf =~ s/(.)\./$1_/g; # directory name fixed
+      if ($_ ne $dnf) {
+        my $tc = 0;
+        while (-d $dnf) {
+          $tc++;
+          $dnf = $dnf."_".$tc;  # - added a count if target exists
+        }
+        move $_, $dnf;
+        # print "$_\n$dnf\n"; # - reports the changes done
+      }
+    }
+  };
+} # change . to _ in directory names - repeat for deeper levels
+
 sub allfods {
-	my ($fodbn,$dir,$ext) = fileparse($_, qr/\.[^.]*/);
-	my $fod0 = $dir.$fodbn.$ext;
-	if (-f $_) {$ext = lc($ext)};
-	$fodbn =~ s/\s+-/-/g;   # - stripped out blanks before hyphens
-	$fodbn =~ s/-\s+/-/g;   # - stripped out blanks after hyphens
-	$fodbn =~ s/\s+_/_/g;   # - stripped out blanks before underscores
-	$fodbn =~ s/_\s+/_/g;   # - stripped out blanks after underscores
-	$fodbn =~ s/\s+$//g;   # - stripped off trailing blanks
-	$fodbn =~ s/\s+/_/g;   # - converted all inner blanks to underscores
-	$ext =~ s/\.\s+/\./g;  # - took out any weirdly present blanks
-	my $fod1 = $dir.$fodbn.$ext;
-	my $ec = 0;
-	if ($fod0 ne $fod1) {
-		while (-e $fod1) {
-			$ec++;
-			$fod1 = $dir.$fodbn."_".$ec.$ext;  # - added a count if target exists
-		}
-		if ($resp eq "g") { move $fod0, $fod1; }
-		print "$fod1\n";  # - reports the changes done
-	}
-}
+  my ($fodbn,$dir,$ext) = fileparse($_, qr/\.[^.]*/); # $_  defined by  File::Find
+  my $fod0 = $dir.$fodbn.$ext;
+  if (-f $_) {$ext = lc($ext)}; # lowercase
+  $fodbn =~ s/\s+-/-/g; # strip out blanks before hyphens
+  $fodbn =~ s/-\s+/-/g; # strip out blanks after hyphens
+  $fodbn =~ s/\s+_/_/g; # strip out blanks before underscores
+  $fodbn =~ s/_\s+/_/g; # strip out blanks after underscores
+  $fodbn =~ s/\s+$//g; # strip off trailing blanks
+  $fodbn =~ s/\s+/_/g; # convert all inner blanks to underscores
+  $fodbn =~ s/à/a/g; # remove French stuff
+  $fodbn =~ s/è/e/g; # remove French stuff
+  $fodbn =~ s/É/E/g; # remove French stuff
+  $fodbn =~ s/é/e/g; # remove French stuff
+  $fodbn =~ s/ê/e/g; # remove French stuff
+  $fodbn =~ s/ï/i/g; # remove French stuff
+  $fodbn =~ s/'/_/g; # remove French stuff
+  $fodbn =~ s///g; # ZNc
+  $ext =~ s/\.\s+/\./g;  # - take out any weirdly present blanks
+  my $fod1 = $dir.$fodbn.$ext;
+  if ($fod0 ne $fod1) {
+    my $tc = 0;
+    while (-e $fod1) {
+      $tc++;
+      $fod1 = $dir.$fodbn."_".$tc.$ext;  # - added a count if target exists
+    }
+    move $fod0, $fod1;
+    # print "$fod1\n"; # - reports the changes done
+  }
+} # fix all file or directory names
+
